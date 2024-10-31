2855ec7c7494c6ee3b378f2a117201cf9d801d0a
Matrix-6969 <ROCKYXXXYASH69@GMAIL.COM>
2024-10-31 17:36:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
